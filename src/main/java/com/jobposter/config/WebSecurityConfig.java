0edf712933dca3e41cc13f2e051540d3101e5ae3
Rofi <rofituluss2@gmail.com>
2020-01-21 17:04:56 +0700

    web security commit

--- src/main/java/com/jobposter/config/WebSecurityConfig.java
+++ src/main/java/com/jobposter/config/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/admin/*/*/*/*","/apl/*/*/*/*").permitAll().
+				.authorizeRequests().antMatchers("/apl/*","/admin/*","/apl/*/*","/admin/*/*","/admin/*/*/*","/apl/*/*/*","/apl/*/*/*/*","/admin/*/*/*/*").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
